a3aad0588d62f5e4aab3830ef34d51d55212ea02
arjunkr2001 <arjunradhakrishnan2001@gmail.com>
2022-05-27 18:44:59 +0530

    first commit on CounterApp

--- App.js
+++ App.js
@@ -1,20 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, Button, Alert, Pressable } from 'react-native';
 
 export default function App() {
+  let [num, setNum] = useState(0)
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={{fontSize:50}}>{num}</Text>
+      <Pressable
+        style={{
+          //width:50,
+          //height:50,
+          borderRadius:50,
+          backgroundColor:'grey',
+          justifyContent:'center',
+          alignItems:'center',
+          marginTop:10,
+          padding:20,
+          fontSize:25,
+          fontWeight:'bold'
+        }}
+        onPress={()=>{setNum(num+1)}}
+      >
+      +
+      </Pressable>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+let styles = StyleSheet.create({
+  container:{
+    flex:1,
+    justifyContent:'center',
+    alignItems:'center',
+    backgroundColor:'pink'
+  }
+})
